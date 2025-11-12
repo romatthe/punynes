7464cd7f7530cf5655552b81c1241a3f2f00ec79
Robin Mattheussen <me@romatthe.dev>
2025-11-12 01:51:42 +0100

    Initial commit

---
+++ README.md
@@ -0,0 +1,8 @@
+<div align="center">
+<samp>
+
+# PunyNES 
+
+</samp>
+</div>
+
---
+++ flake.nix
@@ -0,0 +1,39 @@
+{
+  description = "Rust development environment";
+
+  inputs = {
+    nixpkgs.url = "github:NixOS/nixpkgs/nixos-unstable";
+    flake-parts.url = "github:hercules-ci/flake-parts";
+    rust-overlay = {
+      url = "github:oxalica/rust-overlay";
+      inputs.nixpkgs.follows = "nixpkgs";
+    };
+  };
+
+  outputs = inputs@{ flake-parts, nixpkgs, rust-overlay, ... }:
+    flake-parts.lib.mkFlake { inherit inputs; } {
+      systems = [ "x86_64-linux" "aarch64-linux" ];
+
+      perSystem = { system, ... }:
+        let
+          pkgs = import nixpkgs {
+            inherit system;
+	    overlays = [ (import rust-overlay) ];
+          };
+          toolchain = pkgs.rust-bin.fromRustupToolchainFile ./rust-toolchain.toml;
+        in 
+	{
+          devShells.default = pkgs.mkShell {
+            packages = with pkgs; [
+              toolchain 
+            ];
+
+            shellHook = ''
+              echo "Rust version: $(rustc --version)"
+            '';
+          };
+        };
+      };
+    
+}
+
