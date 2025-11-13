3500d57482f31291614bce88c35936a3a221f7b3
Robin Mattheussen <me@romatthe.dev>
2025-11-13 03:40:07 +0100

    Add a few addressing modes

--- punynes-core/src/cpu/addressing.rs
+++ punynes-core/src/cpu/addressing.rs
@@ -7,12 +7,31 @@ pub enum Operand {
     Address(u16),
 }
 
-// Each addressing mode is a function taking &mut Cpu and returning Operand.
+/// Each addressing mode is a function taking &mut Cpu and returning Operand (which
+/// can be a u8 or u16).
 pub type AddrModeFn = fn(&mut Cpu) -> Operand;
 
+/// Implied mode addressing
+///
+/// The operand value is implicitly defined by the instruction. We return anything useful.
+fn mode_imp(cpu: &mut Cpu) -> Operand {
+    cpu.pc = cpu.pc.wrapping_add(1);
+    Operand::Value(0) // TODO: Is there a better alternative? Adding an enum adds a match arm.
+}
+
+/// Accumulator mode addressing
+///
+/// The value is simply what is stored in the accumulator.
+fn mode_acc(cpu: &mut Cpu) -> Operand {
+    cpu.pc = cpu.pc.wrapping_add(1);
+    Operand::Value(cpu.a)
+}
+
 /// Immediate mode addressing
+///
+/// The next byte is used as the value.
 fn mode_imm(cpu: &mut Cpu) -> Operand {
     let val = cpu.read(cpu.pc + 1);
-    cpu.pc += 2;
+    cpu.pc = cpu.pc.wrapping_add(2);
     Operand::Value(val)
 }
